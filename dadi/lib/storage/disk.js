49dd066d707769fd030696d7f1ae4e626656c4c2
James Lambie <jameslambie@gmail.com>
2016-04-06 08:40:04 +0000

    fix: use full path for the case when images are within subfolders

--- dadi/lib/storage/disk.js
+++ dadi/lib/storage/disk.js
@@ -1,47 +1,59 @@
 var fs = require('fs');
+var nodeUrl = require('url');
 var path = require('path');
 var Promise = require('bluebird');
 var _ = require('underscore');
 
-var config = require(__dirname + '/../config');
+var config = require(__dirname + '/../../../config');
 
-module.exports = function (url) {
+var DiskStorage = function (url) {
   var self = this;
 
-  this.url = url;
+  this.url = nodeUrl.parse(url, true).pathname;
   this.path = path.resolve(config.get('images.directory.path'));
 
   this.getFullUrl = function() {
     return path.join(self.path, self.url.replace('/disk', ''))
   }
-
-  this.get = function () {
-    return new Promise(function(resolve, reject) {
-      var stream = fs.createReadStream(self.getFullUrl());
-
-      stream.on('open', function () {
-        resolve(stream);
-      })
-
-      stream.on('error', function() {
-        reject('File Not Found');
-      })
-
-
-        // var stats = fs.statSync(url);
-        // var fileSize = parseInt(stats["size"]);
-        // if(fileSize === 0) {
-        //   //return help.displayErrorPage(404, 'File size is 0 byte.', res);
-        // }
-//        var tmpReadStream = fs.createReadStream(url);
-      //   imagesize(tmpReadStream, function(err, imageInfo) {
-      //     self.convertAndSave(fsReadStream, imageInfo, originFileName, newFileName, options, returnJSON, res);
-      //   });
-      // }
-      // else {
-      //   //help.displayErrorPage(404, 'File "' + url + '" doesn\'t exist.', res);
-      // }
-
-	  });
-  }
 }
+
+DiskStorage.prototype.get = function () {
+  var self = this;
+  console.log(self.getFullUrl())
+  return new Promise(function(resolve, reject) {
+    // attempt to open
+    var stream = fs.createReadStream(self.getFullUrl());
+
+    stream.on('open', function () {
+      // check file size
+      var stats = fs.statSync(self.getFullUrl());
+      var fileSize = parseInt(stats.size);
+
+      if (fileSize === 0) {
+        var err = {
+          statusCode: 404,
+          message: 'File size is 0 bytes'
+        }
+
+        return reject(err);
+      }
+
+      return resolve(stream);
+    })
+
+    stream.on('error', function() {
+      var err = {
+        statusCode: 404,
+        message: 'File not found'
+      }
+
+      return reject(err);
+    })
+  })
+}
+
+module.exports = function (url) {
+  return new DiskStorage(url);
+}
+
+module.exports.DiskStorage = DiskStorage
