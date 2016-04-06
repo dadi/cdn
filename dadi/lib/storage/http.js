83e9e40432ef2a7b425f4c994e9c79467c73957c
James Lambie <jameslambie@gmail.com>
2016-04-06 08:42:04 +0000

    refactor: simplify the remote request

--- dadi/lib/storage/http.js
+++ dadi/lib/storage/http.js
@@ -1,51 +1,53 @@
 var Promise = require('bluebird');
 var Readable = require('stream').Readable;
 var request = require('request');
-var nodeUrl = require('url');
+var urljoin = require('url-join');
 var _ = require('underscore');
 
-var config = require(__dirname + '/../config');
+var config = require(__dirname + '/../../../config');
 
-module.exports = function (url) {
+var HTTPStorage = function (url) {
   var self = this;
 
   this.url = url;
   this.baseUrl = config.get('images.remote.path');
 
   this.getFullUrl = function() {
-    return nodeUrl.resolve(self.baseUrl, self.url.replace('/remote', ''))
-  }
-
-  this.get = function () {
-    return new Promise(function(resolve, reject) {
-      request({ url: self.getFullUrl() }, function (err, res, body) {
-        if (err) {
-          reject(err)
-        }
-
-        if (res.statusCode === 200) {
-          var stream = new Readable();
-          stream.push(body);
-          stream.push(null);
-
-          resolve(stream)
-        }
-        else {
-          reject(res.statusCode)
-        }
-        //if (!err && res.statusCode === 200) {
-          // var size = parseInt(response.headers['content-length']);
-          // if(size === 0) {
-          //   return help.displayErrorPage(404, 'File size is 0 byte.', res);
-          // }
-          //var tmpReadStream = request({url: url});
-
-        //}
-        //else {
-          //help.displayErrorPage(404, 'Image path "' + url + '" isn\'t valid.', res);
-
-        //}
-      })
-	  })
+    return urljoin(self.baseUrl, self.url.replace('/http', ''))
   }
 }
+
+HTTPStorage.prototype.get = function () {
+  var self = this;
+  console.log(self.getFullUrl())
+  return new Promise(function(resolve, reject) {
+    request
+    .get(self.getFullUrl())
+    .on('response', function(response) {
+      if (response.statusCode === 200) {
+        return resolve(response)
+      }
+      else {
+        var err = {
+          statusCode: response.statusCode,
+          message: response.statusMessage + ': ' + self.getFullUrl()
+        }
+
+        return reject(err)
+      }
+    })
+    .on('error', function(err) {
+      return reject(err)
+    })
+    // var size = parseInt(response.headers['content-length']);
+    // if(size === 0) {
+    //   return help.displayErrorPage(404, 'File size is 0 byte.', res);
+    // }
+  })
+}
+
+module.exports = function (url) {
+  return new HTTPStorage(url);
+}
+
+module.exports.HTTPStorage = HTTPStorage
