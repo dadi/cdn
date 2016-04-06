74a300edb0ad2f00183f401681ae0eeb7f788c81
James Lambie <jameslambie@gmail.com>
2016-04-06 08:41:05 +0000

    refactor: parse the url before assigning it to the storage adapter

--- dadi/lib/storage/factory.js
+++ dadi/lib/storage/factory.js
@@ -1,17 +1,17 @@
+var nodeUrl = require('url');
 var _ = require('underscore');
 var S3Storage = require(__dirname + '/s3');
 var DiskStorage = require(__dirname + '/disk');
 var HTTPStorage = require(__dirname + '/http');
 
-var config = require(__dirname + '/../config');
+var config = require(__dirname + '/../../../config');
 
 module.exports = {
   create: function create(req) {
-    // set a default adapter
-    var adapterKey = 'disk'
-
     // set a default version
     var version = 'v1'
+
+    var url = req.url
 
     // get version from the request header if supplied
     var versionRegex = /vnd.dadicdn-(.*)\+json/
@@ -27,28 +27,34 @@ module.exports = {
         if (block.enabled) {
           if (key === 'directory') return 'disk'
           if (key === 'remote') return 'http'
+          if (key === 's3') return 's3'
           return key;
         }
       }))
 
       if (_.isArray(adapterKey)) adapterKey = adapterKey[0]
+
+      url = url.split('/').slice(18).join('/')
     }
 
     // get storage adapter from the first part of the url
-    if (version === 'v2') adapterKey = _.compact(req.url.split('/')).shift()
+    if (version === 'v2') {
+      adapterKey = _.compact(req.url.split('/')).shift()
+      url = nodeUrl.parse(url, true).pathname
+    }
 
     switch (adapterKey) {
       case 'disk':
-        return new DiskStorage(req.url)
+        return new DiskStorage(url)
         break
-      case 'remote':
-        return new HTTPStorage(req.url)
+      case 'http':
+        return new HTTPStorage(url)
         break
       case 's3':
-        return new S3Storage(req.url)
+        return new S3Storage(url)
         break
       default:
-        return new DiskStorage(req.url)
+        return new DiskStorage(url)
         break
     }
   }
