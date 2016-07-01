9987ec8e02554bf2560c487309e285b95e3fbdaf
James Lambie <jameslambie@gmail.com>
2016-07-01 22:21:15 +0800

    chore: coverage script, again

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -1,47 +1,44 @@
 #! /usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs')
+var path = require('path')
 
-var coberturaBadger = require('istanbul-cobertura-badger');
+var coberturaBadger = require('istanbul-cobertura-badger')
 
 var opts = {
-  badgeFileName: "coverage",
+  badgeFileName: 'coverage',
   destinationDir: __dirname,
-  istanbulReportFile: path.resolve(__dirname + "/../coverage", "cobertura-coverage.xml"),
+  istanbulReportFile: path.resolve(__dirname + '/../coverage', 'cobertura-coverage.xml'),
   thresholds: {
     excellent: 90, // overall percent >= excellent, green badge
     good: 60 // overall percent < excellent and >= good, yellow badge
-    // overall percent < good, red badge
+  // overall percent < good, red badge
   }
-};
+}
 
-//console.log(opts);
+// console.log(opts)
 
 // Load the badge for the report$
-coberturaBadger(opts, function parsingResults(err, badgeStatus) {
+coberturaBadger(opts, function parsingResults (err, badgeStatus) {
   if (err) {
-    console.log("An error occurred: " + err.message);
+    console.log('An error occurred: ' + err.message)
   }
 
-  console.log(badgeStatus);
+  console.log(badgeStatus)
 
-  var readme = path.resolve(__dirname + '/../README.md');
+  var readme = path.resolve(__dirname + '/../README.md')
   var badgeUrl = badgeStatus.url; // e.g. http://img.shields.io/badge/coverage-60%-yellow.svg
 
   // open the README.md and add this url
   fs.readFile(readme, {encoding: 'utf-8'}, function (err, body) {
-      body = body.replace(/(!\[coverage\]\()(.+?)(\))/g, function(whole, a, b, c) {
-        return a + b + '?style=flat-square' + c;
-      });
+    body = body.replace(/(!\[coverage\]\()(.+?)\?*(\))/g, function (whole, a, b, c) {
+      return a + badgeUrl + '?style=flat-square' + c
+    })
 
-      fs.writeFile(readme, body, {encoding: 'utf-8'}, function (err) {
-        if (err) console.log(err.toString());
+    fs.writeFile(readme, body, {encoding: 'utf-8'}, function (err) {
+      if (err) console.log(err.toString())
 
-        console.log("Coverage badge successfully added to " + readme);
-      });
+      console.log('Coverage badge successfully added to ' + readme)
+    })
   })
-
-  //console.log(badgeStatus);
-});
-
+})
