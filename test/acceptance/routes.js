d256590a7b2ade6bc2c410352d19c165cdd31977
James Lambie <jameslambie@gmail.com>
2016-10-07 14:11:24 +0800

    tests: increase timeout before testing result

--- test/acceptance/routes.js
+++ test/acceptance/routes.js
@@ -158,6 +158,8 @@ describe('Routes', function () {
   })
 
   describe('Apply', function () {
+    this.timeout(4000)
+
     it('should evaluate route branches', function (done) {
       var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'))
       var factory = require(__dirname + '/../../dadi/lib/handlers/factory')
@@ -168,14 +170,12 @@ describe('Routes', function () {
       client
       .get('/' + sample.route + '/test.jpg')
       .end(function(err, res) {
-
         setTimeout(function() {
           processBranchesSpy.calledTwice.should.eql(true)
           JSON.stringify(processBranchesSpy.firstCall.args[0]).should.eql(JSON.stringify(sample.branches))
           JSON.stringify(processBranchesSpy.secondCall.args[0]).should.eql(JSON.stringify(sample.branches))
-
           done()
-        }, 1000)
+        }, 2000)
       })
     })
   })
