160bff4118f5413fae898d45975c990255d7ea2a
James Lambie <jameslambie@gmail.com>
2016-10-07 16:53:05 +0800

    fix: switch to different language parser

--- dadi/lib/models/route.js
+++ dadi/lib/models/route.js
@@ -1,4 +1,3 @@
-var acceptLanguage = require('accept-language')
 var fs = require('fs')
 var logger = require('@dadi/logger')
 var path = require('path')
@@ -6,16 +5,6 @@ var request = require('request-promise')
 
 var cache = require(path.join(__dirname, '/../cache'))()
 var config = require(path.join(__dirname, '/../../../config'))
-
-acceptLanguage.languages(['af-ZA', 'sq-AL', 'ar-DZ', 'ar-BH', 'ar-EG', 'ar-IQ', 'ar-JO', 'ar-KW', 'ar-LB', 'ar-LY', 'ar-MA', 'ar-OM', 'ar-QA',
-'ar-SA', 'ar-SY', 'ar-TN', 'ar-AE', 'ar-YE', 'hy-AM', 'eu-ES', 'be-BY', 'bg-BG', 'ca-ES', 'zh-CN', 'zh-HK', 'zh-MO',
-'zh-SG', 'zh-TW', 'zh-CHS', 'zh-CHT', 'hr-HR', 'cs-CZ', 'da-DK', 'div-MV', 'nl-BE', 'nl-NL', 'en-AU', 'en-BZ', 'en-CA',
-'en-CB', 'en-IE', 'en-JM', 'en-NZ', 'en-PH', 'en-ZA', 'en-TT', 'en-GB', 'en-US', 'en-ZW', 'et-EE', 'fo-FO', 'fa-IR',
-'fi-FI', 'fr-BE', 'fr-CA', 'fr-FR', 'fr-LU', 'fr-MC', 'fr-CH', 'gl-ES', 'ka-GE', 'de-AT', 'de-DE', 'de-LI', 'de-LU', 'de-CH', 'el-GR', 'gu-IN',
-'he-IL', 'hi-IN', 'hu-HU', 'is-IS', 'id-ID', 'it-IT', 'it-CH', 'ja-JP', 'kn-IN', 'kk-KZ', 'kok-IN', 'ko-KR', 'ky-KZ', 'lv-LV', 'lt-LT', 'mk-MK',
-'ms-BN', 'ms-MY', 'mr-IN', 'mn-MN', 'nb-NO', 'nn-NO', 'pl-PL', 'pt-BR', 'pt-PT', 'pa-IN', 'ro-RO', 'ru-RU', 'sa-IN', 'sk-SK', 'sl-SI', 'es-AR',
-'es-BO', 'es-CL', 'es-CO', 'es-CR', 'es-DO', 'es-EC', 'es-SV', 'es-GT', 'es-HN', 'es-MX', 'es-NI', 'es-PA', 'es-PY', 'es-PE', 'es-PR',
-'es-ES', 'es-UY', 'es-VE', 'sw-KE', 'sv-FI', 'sv-SE', 'syr-SY', 'ta-IN', 'tt-RU', 'te-IN', 'th-TH', 'tr-TR', 'uk-UA', 'ur-PK', 'vi-VN'])
 
 var Route = function (config) {
   this.config = config
@@ -172,12 +161,16 @@ Route.prototype.getDevice = function () {
 }
 
 Route.prototype.getLanguages = function (minQuality) {
-  var language = acceptLanguage.get(this.language)
-  if (!(language instanceof Array)) {
-    language = [language]
-  }
+  var languages = require('accept-language-parser').parse(this.language)
+  var result = []
 
-  return language
+  languages.forEach((language) => {
+    if ((result.indexOf(language.code) === -1) && (language.quality >= minQuality)) {
+      result.push(language.code)
+    }
+  })
+
+  return result
 }
 
 Route.prototype.getLocation = function () {
