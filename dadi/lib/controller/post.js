186395ab0c3f3b927d19510a60d51ea2cf736f55
Adam K Dean <adamkdean@googlemail.com>
2017-01-27 14:46:34 +0000

    test: remove whitespace padding from post controller

--- dadi/lib/controller/post.js
+++ dadi/lib/controller/post.js
@@ -24,7 +24,6 @@ PostController.prototype.post = (req, res) => {
 
   // Listen for event when Busboy finds a file to stream
   busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-
     this.fileName = filename.replace(/[^a-z0-9\-_.]+/gi, '_')
     this.mimetype = mimetype
 
