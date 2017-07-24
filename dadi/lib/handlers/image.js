473297c6072fab62afbb755b63b88a6e5c5d3871
James Lambie <jameslambie@gmail.com>
2017-07-24 18:25:43 +0100

    chore: standardjs rules

--- dadi/lib/handlers/image.js
+++ dadi/lib/handlers/image.js
@@ -478,25 +478,26 @@ ImageHandler.prototype.convert = function (stream, imageInfo) {
         // sharpening
         if (options.sharpen !== 5) {
           sharpImage = sharpImage.sharpen(options.sharpen)
-        } else if (quality >= 70) {
-          if (/jpe?g/.exec(imageInfo.format) || /jpe?g/.exec(options.format)) {
-            sharpImage = sharpImage.sharpen(1)
-            // TODO: sharpen options for JPG & PNG
-          // } else if (/png/.exec(imageInfo.format) || /png/.exec(options.format)) {
-          //   batch.sharpen(5)
-          } else if (options.cropX && options.cropY) {
-            sharpImage = sharpImage.sharpen(1)
-          }
         }
+        //  else if (quality >= 70) {
+        //   if (/jpe?g/.exec(imageInfo.format) || /jpe?g/.exec(options.format)) {
+        //     sharpImage = sharpImage.sharpen(1)
+        //     // TODO: sharpen options for JPG & PNG
+        //   // } else if (/png/.exec(imageInfo.format) || /png/.exec(options.format)) {
+        //   //   batch.sharpen(5)
+        //   } else if (options.cropX && options.cropY) {
+        //     sharpImage = sharpImage.sharpen(1)
+        //   }
+        // }
 
         // give it a little colour
         // TODO: saturate
         // batch.saturate(options.saturate)
 
         // Image format and parameters
-        var format = (self.options.format === 'json' ?
-          imageInfo.format :
-          self.options.format).toLowerCase()
+        var format = (self.options.format === 'json'
+          ? imageInfo.format
+          : self.options.format).toLowerCase()
 
         var outputFn
         var outputOptions = {}
