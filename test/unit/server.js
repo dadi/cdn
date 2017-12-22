fe1fd64c1d1a34c8d3decc0201bb94c90290b35c
Eduardo Bouças <mail@eduardoboucas.com>
2017-12-22 10:20:55 +0000

    test: add workspace generation to Server

--- test/unit/server.js
+++ test/unit/server.js
@@ -1,6 +1,8 @@
+var path = require('path')
 var should = require('should')
 var sinon = require('sinon')
 var server = require(__dirname + '/../../dadi/lib')
+var Server = require(__dirname + '/../../dadi/lib').Server
 var fs = require('fs')
 
 describe('Server', function () {
@@ -12,7 +14,7 @@ describe('Server', function () {
 
   describe('start', function () {
     it('should set readyState', function (done) {
-      var stub = sinon.stub(fs, 'readdirSync').callsFake(function () { return []; })
+      var stub = sinon.stub(fs, 'readdirSync').callsFake(function () { return [] })
 
       server.start()
 
@@ -26,7 +28,7 @@ describe('Server', function () {
 
   describe('stop', function () {
     it('should set readyState', function (done) {
-      var stub = sinon.stub(server.server, 'close').callsFake(function (cb) { cb(); })
+      var stub = sinon.stub(server.server, 'close').callsFake(function (cb) { cb() })
 
       server.stop(function (err) {
         if (err) return done(err)
@@ -37,6 +39,67 @@ describe('Server', function () {
 
         done()
       })
+    })
+  })
+
+  describe('getWorkspace', function () {
+    it('should return hash of file names', function (done) {
+      var spy = sinon.spy(Server.prototype, 'getWorkspace')
+
+      const s = new Server()
+      const workspace = s.getWorkspace()
+
+      spy.restore()
+      spy.called.should.eql(true)
+
+      var files = spy.firstCall.returnValue
+
+      Object.keys(files).includes('layout').should.eql(true)
+      Object.keys(files).includes('sample-image-recipe').should.eql(true)
+
+      done()
+    })
+
+    it('should include type and path properties', function (done) {
+      var spy = sinon.spy(Server.prototype, 'getWorkspace')
+
+      const s = new Server()
+      const workspace = s.getWorkspace()
+
+      spy.restore()
+      spy.called.should.eql(true)
+
+      var files = spy.firstCall.returnValue
+
+      files['layout'].type.should.eql('plugins')
+      files['layout'].path.should.eql(
+        path.resolve(__dirname + '/../../workspace/plugins/layout.js')
+      )
+      files['sample-asset-recipe'].type.should.eql('recipes')
+      files['sample-asset-recipe'].path.should.eql(
+        path.resolve(__dirname + '/../../workspace/recipes/sample-asset-recipe.json')
+      )
+
+      done()  
+    })
+
+    it('should include source property for recipes', function (done) {
+      var spy = sinon.spy(Server.prototype, 'getWorkspace')
+
+      const s = new Server()
+      const workspace = s.getWorkspace()
+
+      spy.restore()
+      spy.called.should.eql(true)
+
+      var files = spy.firstCall.returnValue
+      var recipePath = path.resolve(__dirname + '/../../workspace/recipes/sample-asset-recipe.json')
+
+      files['sample-asset-recipe'].type.should.eql('recipes')
+      files['sample-asset-recipe'].path.should.eql(recipePath)
+      files['sample-asset-recipe'].source.toString().should.eql(require(recipePath).toString())
+
+      done()
     })
   })
 })
