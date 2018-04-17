a891ac24bf1167908315b116714b195b29b6ca95
Eduardo Bouças <mail@eduardoboucas.com>
2018-04-17 10:01:14 +0100

    chore: remove unused code

--- dadi/lib/help.js
+++ dadi/lib/help.js
@@ -1,8 +1,6 @@
-var path = require('path')
-var streamLength = require('stream-length')
-
-var config = require(path.resolve(path.join(__dirname, '/../../config')))
-var cache = require(path.join(__dirname, '/cache'))
+const path = require('path')
+const config = require(path.resolve(path.join(__dirname, '/../../config')))
+const cache = require(path.join(__dirname, '/cache'))
 
 module.exports.clearCache = function (pathname, callback) {
   cache().delete(pathname, (err) => {
@@ -11,46 +9,18 @@ module.exports.clearCache = function (pathname, callback) {
   })
 }
 
-module.exports.contentLength = function (stream) {
-  return new Promise(function (resolve, reject) {
-    Promise.try(function () {
-      return streamLength(stream)
-    })
-      .then(function (result) {
-        resolve(result)
-      })
-      .catch(function (err) {
-        reject(err)
-      })
-  })
-}
-
 // helper that sends json response
 module.exports.sendBackJSON = function (successCode, results, res) {
   res.statusCode = successCode
 
-  var resBody = JSON.stringify(results)
+  let resBody = JSON.stringify(results)
+
   if (results instanceof Error && resBody === '{}') {
     resBody = JSON.stringify({ message: results.message || 'unknown error' })
   }
 
   res.setHeader('Server', config.get('server.name'))
   res.setHeader('Content-Type', 'application/json')
-  res.setHeader('Content-Length', Buffer.byteLength(resBody))
-  res.end(resBody)
-}
-
-module.exports.sendBackJSONP = function (callbackName, results, res) {
-  // callback MUST be made up of letters only
-  if (!callbackName.match(/^[a-zA-Z]+$/)) return res.send(400)
-
-  res.statusCode = 200
-
-  var resBody = JSON.stringify(results)
-  resBody = callbackName + '(' + resBody + ');'
-
-  res.setHeader('Server', config.get('server.name'))
-  res.setHeader('Content-Type', 'text/javascript')
   res.setHeader('Content-Length', Buffer.byteLength(resBody))
   res.end(resBody)
 }
