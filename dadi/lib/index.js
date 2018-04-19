9fb4c9a330c80a8e444a0e3ed57d0813e947021b
Eduardo Bouças <mail@eduardoboucas.com>
2018-04-19 10:55:08 +0100

    refactor: tidy up server file

--- dadi/lib/index.js
+++ dadi/lib/index.js
@@ -15,12 +15,18 @@ const dadiStatus = require('@dadi/status')
 const domainManager = require('./models/domain-manager')
 const workspace = require('./models/workspace')
 
-// let's ensure there's at least a dev config file here
-const devConfigPath = path.join(__dirname, '/../../config/config.development.json')
+// Let's ensure there's at least a dev config file here.
+const devConfigPath = path.join(
+  __dirname,
+  '/../../config/config.development.json'
+)
 
 fs.stat(devConfigPath, (err, stats) => {
   if (err && err.code === 'ENOENT') {
-    fs.writeFileSync(devConfigPath, fs.readFileSync(devConfigPath + '.sample'))
+    fs.writeFileSync(
+      devConfigPath,
+      fs.readFileSync(devConfigPath + '.sample')
+    )
   }
 })
 
@@ -29,13 +35,15 @@ const Controller = require(path.join(__dirname, '/controller'))
 const configPath = path.resolve(path.join(__dirname, '/../../config'))
 const config = require(configPath)
 
-function createServer (listener) {
-  var protocol = config.get('server.protocol')
+const Server = function () {}
+
+Server.prototype.create = function (listener) {
+  let protocol = config.get('server.protocol')
 
   if (protocol === 'http') {
     return http.createServer(listener)
   } else if (protocol === 'https') {
-    var readFileSyncSafe = (path) => {
+    let readFileSyncSafe = (path) => {
       try {
         return fs.readFileSync(path)
       } catch (ex) {
@@ -45,10 +53,10 @@ function createServer (listener) {
       return null
     }
 
-    var passphrase = config.get('server.sslPassphrase')
-    var caPath = config.get('server.sslIntermediateCertificatePath')
-    var caPaths = config.get('server.sslIntermediateCertificatePaths')
-    var serverOptions = {
+    let passphrase = config.get('server.sslPassphrase')
+    let caPath = config.get('server.sslIntermediateCertificatePath')
+    let caPaths = config.get('server.sslIntermediateCertificatePaths')
+    let serverOptions = {
       key: readFileSyncSafe(config.get('server.sslPrivateKeyPath')),
       cert: readFileSyncSafe(config.get('server.sslCertificatePath'))
     }
@@ -59,25 +67,31 @@ function createServer (listener) {
 
     if (caPaths && caPaths.length > 0) {
       serverOptions.ca = []
-      caPaths.forEach((path) => {
-        var data = readFileSyncSafe(path)
-        data && serverOptions.ca.push(data)
+      caPaths.forEach(path => {
+        let data = readFileSyncSafe(path)
+
+        if (data) {
+          serverOptions.ca.push(data)
+        }
       })
     } else if (caPath && caPath.length > 0) {
       serverOptions.ca = readFileSyncSafe(caPath)
     }
 
-    // we need to catch any errors resulting from bad parameters
-    // such as incorrect passphrase or no passphrase provided
+    // We need to catch any errors resulting from bad parameters,
+    // such as incorrect passphrase or no passphrase provided.
     try {
       return https.createServer(serverOptions, listener)
     } catch (ex) {
-      var exPrefix = 'error starting https server: '
+      let exPrefix = 'error starting https server: '
+
       switch (ex.message) {
         case 'error:06065064:digital envelope routines:EVP_DecryptFinal_ex:bad decrypt':
           throw new Error(exPrefix + 'incorrect ssl passphrase')
+
         case 'error:0906A068:PEM routines:PEM_do_header:bad password read':
           throw new Error(exPrefix + 'required ssl passphrase not provided')
+
         default:
           throw new Error(exPrefix + ex.message)
       }
@@ -85,12 +99,12 @@ function createServer (listener) {
   }
 }
 
-function onListening (server) {
-  var env = config.get('env')
-  var address = server.address()
+Server.prototype.onListening = function () {
+  let address = this.address()
+  let env = config.get('env')
 
   if (env !== 'test') {
-    var startText = '\n  ----------------------------\n'
+    let startText = '\n  ----------------------------\n'
     startText += '  Started \'DADI CDN\'\n'
     startText += '  ----------------------------\n'
     startText += '  Server:      '.green + address.address + ':' + address.port + '\n'
@@ -105,12 +119,12 @@ function onListening (server) {
   }
 }
 
-function onRedirectListening (server) {
-  var env = config.get('env')
-  var address = server.address()
+Server.prototype.onRedirectListening = function () {
+  let address = this.address()
+  let env = config.get('env')
 
   if (env !== 'test') {
-    var startText = '\n  ----------------------------\n'
+    let startText = '\n  ----------------------------\n'
     startText += '  Started HTTP -> HTTPS Redirect\n'
     startText += '  ----------------------------\n'
     startText += '  Server:      '.green + address.address + ':' + address.port + '\n'
@@ -120,12 +134,12 @@ function onRedirectListening (server) {
   }
 }
 
-function onStatusListening (server) {
-  var env = config.get('env')
-  var address = server.address()
+Server.prototype.onStatusListening = function () {
+  var address = this.address()
+  let env = config.get('env')
 
   if (env !== 'test') {
-    var startText = '\n  ----------------------------\n'
+    let startText = '\n  ----------------------------\n'
     startText += '  Started standalone status endpoint\n'
     startText += '  ----------------------------\n'
     startText += '  Server:      '.green + address.address + ':' + address.port + '\n'
@@ -134,8 +148,6 @@ function onStatusListening (server) {
     console.log(startText)
   }
 }
-
-var Server = function () {}
 
 Server.prototype.start = function (done) {
   router.use((req, res, next) => {
@@ -155,96 +167,61 @@ Server.prototype.start = function (done) {
     res.end('Welcome to DADI CDN')
   })
 
-  var statusHandler = function (req, res, next) {
-    var method = req.method && req.method.toLowerCase()
-    var authorization = req.headers.authorization
-
-    if (method !== 'post' || config.get('status.enabled') === false) {
-      return next()
-    } else {
-      var params = {
-        site: site,
-        package: '@dadi/cdn',
-        version: version,
-        healthCheck: {
-          authorization: authorization,
-          baseUrl: 'http://' + config.get('server.host') + ':' + config.get('server.port'),
-          routes: config.get('status.routes')
-        }
-      }
-
-      dadiStatus(params, function (err, data) {
-        if (err) return next(err)
-
-        var responseMessages = {
-          Green: 'Service is responding within specified parameters',
-          Amber: 'Service is responding, but outside of specified parameters'
-        }
-
-        data.status = {
-          status: data.routes[0].status,
-          healthStatus: data.routes[0].healthStatus,
-          message: responseMessages[data.routes[0].healthStatus] || 'Service is not responding correctly'
-        }
-
-        var resBody = JSON.stringify(data, null, 2)
-
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
-        res.setHeader('Content-Length', Buffer.byteLength(resBody))
-        return res.end(resBody)
-      })
-    }
-  }
-
-  // ensure that middleware runs in the correct order,
-  // especially when running an integrated status page
+  // Ensure that middleware runs in the correct order,
+  // especially when running an integrated status page.
   if (config.get('status.standalone')) {
-    var statusRouter = Router()
-    config.get('status.requireAuthentication') && auth(statusRouter)
-    statusRouter.use('/api/status', statusHandler)
+    let statusRouter = Router()
 
-    var statusApp = http.createServer(function (req, res) {
+    config.get('status.requireAuthentication') && auth(statusRouter)
+    statusRouter.use('/api/status', this.status)
+
+    let statusApp = http.createServer(function (req, res) {
       res.setHeader('Server', config.get('server.name'))
       res.setHeader('Access-Control-Allow-Origin', '*')
       res.setHeader('Cache-Control', 'no-cache')
+
       statusRouter(req, res, finalhandler(req, res))
     })
 
-    var statusServer = this.statusServer = statusApp.listen(config.get('status.port'))
-    statusServer.on('listening', function () { onStatusListening(this) })
+    let statusServer = statusApp.listen(config.get('status.port'))
+
+    statusServer.on('listening', this.onStatusListening)
+
+    this.statusServer = statusServer
 
     auth(router)
   } else {
     if (config.get('status.requireAuthentication')) {
       auth(router)
-      router.use('/api/status', statusHandler)
+      router.use('/api/status', this.status)
     } else {
-      router.use('/api/status', statusHandler)
+      router.use('/api/status', this.status)
       auth(router)
     }
   }
 
   this.controller = new Controller(router)
 
-  var redirectInstance
-  var redirectServer
-  var redirectPort = config.get('server.redirectPort')
+  let redirectInstance
+  let redirectServer
+  let redirectPort = config.get('server.redirectPort')
+
   if (redirectPort > 0) {
     redirectInstance = http.createServer((req, res) => {
-      var port = config.get('server.port')
-      var hostname = req.headers.host.split(':')[0]
-      var location = 'https://' + hostname + ':' + port + req.url
+      let port = config.get('server.port')
+      let hostname = req.headers.host.split(':')[0]
+      let location = `https://${hostname}:${port}${req.url}`
 
       res.setHeader('Location', location)
       res.statusCode = 301
       res.end()
     })
-    redirectServer = this.redirectServer = redirectInstance.listen(redirectPort)
-    redirectServer.on('listening', function () { onRedirectListening(this) })
+
+    redirectServer = redirectInstance.listen(redirectPort)
+    redirectServer.on('listening', this.onRedirectListening)
   }
 
-  let app = createServer((req, res) => {
+  let app = this.create((req, res) => {
     if (config.get('multiDomain.enabled')) {
       let domain = req.headers.host.split(':')[0]
 
@@ -268,10 +245,11 @@ Server.prototype.start = function (done) {
     router(req, res, finalhandler(req, res))
   })
 
-  let server = this.server = app.listen(config.get('server.port'))
-  server.on('listening', function () { onListening(this) })
+  let server = app.listen(config.get('server.port'))
+  server.on('listening', this.onListening)
 
   this.readyState = 1
+  this.server = server
 
   workspace.createDirectories()
   workspace.startWatchingFiles()
@@ -281,24 +259,72 @@ Server.prototype.start = function (done) {
   }
 }
 
-// this is mostly needed for tests
+Server.prototype.status = function (req, res, next) {
+  let method = req.method && req.method.toLowerCase()
+  let authorization = req.headers.authorization
+
+  if (method !== 'post' || config.get('status.enabled') === false) {
+    return next()
+  }
+
+  let params = {
+    site: site,
+    package: '@dadi/cdn',
+    version: version,
+    healthCheck: {
+      authorization: authorization,
+      baseUrl: `http://${config.get('server.host')}:${config.get('server.port')}`,
+      routes: config.get('status.routes')
+    }
+  }
+
+  dadiStatus(params, (err, data) => {
+    if (err) return next(err)
+
+    let responseMessages = {
+      Green: 'Service is responding within specified parameters',
+      Amber: 'Service is responding, but outside of specified parameters'
+    }
+
+    data.status = {
+      status: data.routes[0].status,
+      healthStatus: data.routes[0].healthStatus,
+      message: responseMessages[data.routes[0].healthStatus] ||
+        'Service is not responding correctly'
+    }
+
+    let resBody = JSON.stringify(data, null, 2)
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Content-Length', Buffer.byteLength(resBody))
+
+    return res.end(resBody)
+  })
+}
+
+// This is mostly needed for tests.
 Server.prototype.stop = function (done) {
   this.readyState = 3
 
   workspace.stopWatchingFiles()
 
   this.server.close(err => {
-    // if statusServer is running in standalone, close that too
+    // If statusServer is running in standalone, close that too.
     if (this.statusServer) {
       this.statusServer.close(err => {
         this.readyState = 0
 
-        done && done(err)
+        if (typeof done === 'function') {
+          done(err)
+        }
       })
     } else {
       this.readyState = 0
 
-      done && done(err)
+      if (typeof done === 'function') {
+        done(err)
+      }
     }
   })
 }
