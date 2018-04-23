b2be4075ef9740a63f1b976eda3f11bd00faeb14
Eduardo Bouças <mail@eduardoboucas.com>
2018-04-23 15:04:23 +0100

    feat: allow controller plugins to set cache header

--- test/unit/plugins.js
+++ test/unit/plugins.js
@@ -7,60 +7,62 @@ const stream = require('stream')
 
 const ImageHandler = require(__dirname + '/../../dadi/lib/handlers/image')
 
-describe('Plugins', function (done) {
-  it('should receive the correct parameters', done => {
-    const req = {
-      url: '/test.jpg?width=350&height=530&resizeStyle=entropy'
-    }
-
-    let pluginParameters = {}
-
-    const pluginPost = parameters => {
-      pluginParameters = parameters
-    }
-    const handler = new ImageHandler('jpg', req)
-
-    sinon.stub(handler.storageFactory, 'create').returns({
-      get: () => {
-        const readable = new fs.createReadStream(
-          path.join(path.resolve(config.get('images.directory.path')), '/test.jpg')
-        )
-
-        return Promise.resolve(readable)        
+describe('Plugins', done => {
+  describe('post-processing', () => {
+    it('should receive the correct parameters', done => {
+      const req = {
+        url: '/test.jpg?width=350&height=530&resizeStyle=entropy'
       }
-    })
 
-    handler.plugins = [{
-      post: pluginPost
-    }]
+      let pluginParameters = {}
 
-    handler.get().then(response => {
-      handler.storageFactory.create.restore()
+      const pluginPost = parameters => {
+        pluginParameters = parameters
+      }
+      const handler = new ImageHandler('jpg', req)
 
-      pluginParameters.assetStore.should.be.Function
+      sinon.stub(handler.storageFactory, 'create').returns({
+        get: () => {
+          const readable = new fs.createReadStream(
+            path.join(path.resolve(config.get('images.directory.path')), '/test.jpg')
+          )
 
-      pluginParameters.cache.get.should.be.Function
-      pluginParameters.cache.set.should.be.Function
+          return Promise.resolve(readable)        
+        }
+      })
 
-      pluginParameters.imageInfo.format.should.eql('jpg')
-      pluginParameters.imageInfo.width.should.eql(350)
-      pluginParameters.imageInfo.height.should.eql(530)
-      pluginParameters.imageInfo.naturalWidth.should.be.Number
-      pluginParameters.imageInfo.naturalHeight.should.be.Number
+      handler.plugins = [{
+        post: pluginPost
+      }]
 
-      pluginParameters.options.format.should.eql('jpg')
-      pluginParameters.options.width.should.eql(350)
-      pluginParameters.options.height.should.eql(530)
-      pluginParameters.options.height.should.eql(530)
-      pluginParameters.options.resizeStyle.should.eql('entropy')
+      handler.get().then(response => {
+        handler.storageFactory.create.restore()
 
-      pluginParameters.processor.constructor.name.should.eql('Sharp')
+        pluginParameters.assetStore.should.be.Function
 
-      pluginParameters.sharp.should.be.Function
+        pluginParameters.cache.get.should.be.Function
+        pluginParameters.cache.set.should.be.Function
 
-      pluginParameters.url.should.eql(req.url)
+        pluginParameters.imageInfo.format.should.eql('jpg')
+        pluginParameters.imageInfo.width.should.eql(350)
+        pluginParameters.imageInfo.height.should.eql(530)
+        pluginParameters.imageInfo.naturalWidth.should.be.Number
+        pluginParameters.imageInfo.naturalHeight.should.be.Number
 
-      done()
+        pluginParameters.options.format.should.eql('jpg')
+        pluginParameters.options.width.should.eql(350)
+        pluginParameters.options.height.should.eql(530)
+        pluginParameters.options.height.should.eql(530)
+        pluginParameters.options.resizeStyle.should.eql('entropy')
+
+        pluginParameters.processor.constructor.name.should.eql('Sharp')
+
+        pluginParameters.sharp.should.be.Function
+
+        pluginParameters.url.should.eql(req.url)
+
+        done()
+      })
     })
   })
 })
