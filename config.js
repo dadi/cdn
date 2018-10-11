c49a0647db4ae7fffde3628ea1eeea6c948511f2
James Lambie <jameslambie@gmail.com>
2018-10-11 10:45:04 +0100

    feat: reload domain configs on directory changes

--- config.js
+++ config.js
@@ -647,7 +647,21 @@ const Config = function () {
   this.domainSchema = {}
   this.createDomainSchema(schema, this.domainSchema)
 
-  this.loadDomainConfigs()
+  let domainsDirectory = this.get('multiDomain.directory')
+
+  // Watch the domains directory for new & removed domain configurations.
+  this.domainsWatcher = chokidar.watch(domainsDirectory, {
+    awaitWriteFinish: true,
+    depth: 1,
+    usePolling: true
+  }).on('addDir', (event, filePath) => {
+    this.loadDomainConfigs(domainsDirectory)
+  }).on('unlinkDir', (event, filePath) => {
+    // Wait 3 sec for the delete to finish before rescanning
+    setTimeout(() => {
+      this.loadDomainConfigs(domainsDirectory)
+    }, 3000)
+  })
 }
 
 Config.prototype = convict(schema)
@@ -733,13 +747,12 @@ Config.prototype.get = function (path, domain) {
  *
  * @return {Object}
  */
-Config.prototype.loadDomainConfigs = function () {
+Config.prototype.loadDomainConfigs = function (domainsDirectory) {
   if (!this.get('multiDomain.enabled')) {
     return {}
   }
 
   let configs = {}
-  let domainsDirectory = this.get('multiDomain.directory')
 
   domainManager
     .scanDomains(domainsDirectory)
