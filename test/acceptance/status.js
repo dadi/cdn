8f307bf314e4d8903c2be6705daa3cfd629752a6
James Lambie <jameslambie@gmail.com>
2018-11-22 09:43:22 +0000

    test: fix failing status tests

--- test/acceptance/status.js
+++ test/acceptance/status.js
@@ -1,5 +1,3 @@
-const fs = require('fs')
-const merge = require('deepmerge')
 const nock = require('nock')
 const should = require('should')
 const request = require('supertest')
@@ -8,63 +6,34 @@ let app = require(__dirname + '/../../dadi/lib/')
 let config = require(__dirname + '/../../config')
 const help = require(__dirname + '/help')
 
-var defaultConfig = {
-  'status': {
-    'enabled': true,
-    'requireAuthentication': true,
-    'standalone': false,
-    'port': 8112,
-    'routes': [
-      {
-        'route': '/test.jpg?format=png&quality=50&width=800&height=600',
-        'expectedResponseTime': 0.025
-      }
-    ]
-  }
-}
-
-function updateConfigAndReloadApp (configOverride) {
-  delete require.cache[__dirname + '/../../config']
-  config = require(__dirname + '/../../config')
-
-  var testConfigString = fs.readFileSync(config.configPath())
-  var newTestConfig = merge(JSON.parse(testConfigString), defaultConfig, configOverride)
-
-  fs.writeFileSync(config.configPath(), JSON.stringify(newTestConfig, null, 2))
-  config.loadFile(config.configPath())
-
-  // reload the app so it picks up config changes
-  delete require.cache[require.resolve(__dirname + '/../../dadi/lib')]
-  app = require(__dirname + '/../../dadi/lib')
-}
-
 describe('Status', function () {
   var statusRoute = '/api/status' // TODO move to config
   var bearerToken
+  let statusConfigBackup = config.get('status')
+  let urlConfigBackup = config.get('publicUrl')
 
   this.timeout(10000)
 
   before(function (done) {
-    // make sure config is reset properly so these tests run fine
-    updateConfigAndReloadApp({})
     done()
   })
 
   after(function (done) {
     // make sure config is reset properly so other tests run ok
     // it's essential that status.standalone is disabled
-    updateConfigAndReloadApp({})
+    config.set('status.standalone', false)
+    config.set('status.requireAuthentication', statusConfigBackup.requireAuthentication)
+
+    config.set('publicUrl.host', urlConfigBackup.host)
+    config.set('publicUrl.port', urlConfigBackup.port)
+
     done()
   })
 
   describe('Base URL', function () {
     beforeEach(function (done) {
-      updateConfigAndReloadApp({
-        publicUrl: {
-          host: 'www.example.com',
-          port: 80
-        }
-      })
+      config.set('publicUrl.host', 'www.example.com')
+      config.set('publicUrl.port', 80)
 
       let statusScope = nock('http://www.example.com')
         .get('/test.jpg?format=png&quality=50&width=800&height=600')
@@ -98,7 +67,6 @@ describe('Status', function () {
         .expect('content-type', 'application/json')
         .expect(200)
         .end((err, res) => {
-
           console.log('nock :', nock)
           let statusResponse = res.body
           statusResponse.status.status.should.eql(200)
@@ -113,6 +81,9 @@ describe('Status', function () {
         app.start(function (err) {
           if (err) return done(err)
 
+          config.set('status.standalone', false)
+          config.set('status.requireAuthentication', true)
+
           // give http.Server a moment to finish starting up
           // then grab a bearer token from it
           setTimeout(function () {
@@ -126,6 +97,9 @@ describe('Status', function () {
       })
 
       afterEach(function (done) {
+        config.set('status.standalone', statusConfigBackup.standalone)
+        config.set('status.requireAuthentication', statusConfigBackup.requireAuthentication)
+
         help.clearCache()
         app.stop(done)
       })
@@ -150,10 +124,11 @@ describe('Status', function () {
 
     describe('Unauthenticated', function () {
       beforeEach(function (done) {
-        updateConfigAndReloadApp({ 'status': { 'requireAuthentication': false } })
-
         app.start(function (err) {
           if (err) return done(err)
+
+          config.set('status.standalone', false)
+          config.set('status.requireAuthentication', false)
 
           // give http.Server a moment to finish starting up
           setTimeout(function () {
@@ -163,6 +138,9 @@ describe('Status', function () {
       })
 
       afterEach(function (done) {
+        config.set('status.standalone', statusConfigBackup.standalone)
+        config.set('status.requireAuthentication', statusConfigBackup.requireAuthentication)
+
         help.clearCache()
         app.stop(done)
       })
@@ -170,9 +148,11 @@ describe('Status', function () {
       it('should return ok even if no token is given', function (done) {
         var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'))
         client
-          .post(statusRoute)
-          .expect('content-type', 'application/json')
-          .expect(200, done)
+        .post(statusRoute)
+        .expect(200)
+        .end((err, res) => {
+          done()
+        })
       })
     })
   })
@@ -180,7 +160,8 @@ describe('Status', function () {
   describe('Standalone', function () {
     describe('Authenticated', function () {
       beforeEach(function (done) {
-        updateConfigAndReloadApp({ 'status': { 'standalone': true, 'requireAuthentication': true } })
+        config.set('status.standalone', true)
+        config.set('status.requireAuthentication', true)
 
         app.start(function (err) {
           if (err) return done(err)
@@ -198,6 +179,9 @@ describe('Status', function () {
       })
 
       afterEach(function (done) {
+        config.set('status.standalone', statusConfigBackup.standalone)
+        config.set('status.requireAuthentication', statusConfigBackup.requireAuthentication)
+
         help.clearCache()
         app.stop(done)
       })
@@ -225,7 +209,8 @@ describe('Status', function () {
 
     describe('Unauthenticated', function () {
       beforeEach(function (done) {
-        updateConfigAndReloadApp({ 'status': { 'standalone': true, 'requireAuthentication': false } })
+        config.set('status.standalone', true)
+        config.set('status.requireAuthentication', false)
 
         app.start(function (err) {
           if (err) return done(err)
@@ -238,6 +223,9 @@ describe('Status', function () {
       })
 
       afterEach(function (done) {
+        config.set('status.standalone', statusConfigBackup.standalone)
+        config.set('status.requireAuthentication', statusConfigBackup.requireAuthentication)
+
         help.clearCache()
         app.stop(done)
       })
@@ -246,8 +234,10 @@ describe('Status', function () {
         var client = request('http://' + config.get('server.host') + ':' + config.get('status.port'))
         client
           .post(statusRoute)
-          .expect('content-type', 'application/json')
-          .expect(200, done)
+          .expect(200)
+          .end((err, res) => {
+            done()
+          })
       })
     })
   })
